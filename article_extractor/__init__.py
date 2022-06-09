92435f0ce6ca242d09f2f2dc9cfe2a1bb8942851
Dimitrii Kutniakov <d.v.kutnyakov@gmail.com>
2022-06-09 11:59:18 +0300

    add extract()

--- article_extractor/__init__.py
+++ article_extractor/__init__.py
@@ -1,1 +1,18 @@
 from .main import Article
+
+
+def extract(url: str, out_dir: str, out_format: str = "docx") -> str:
+    """Extract article by url and save to file.
+
+    Args:
+        url: valid URL to article
+        out_dir: directory to store output file
+        out_format: output file format, check pypandoc.convert_text() documentation
+            for supported formats.
+
+    Returns:
+        str: created file path
+    """
+    article = Article(url)
+    article.fetch()
+    return article.save(out_dir, out_format)
